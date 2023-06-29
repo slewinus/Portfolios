238ce3a237678167d497dac167c18695601af05a
slewinus <oscar.robertbesle@gmail.com>
2023-06-29 17:10:21 +0200

    change footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -107,7 +107,7 @@ const Footer = () => {
           rel="nofollow noopener noreferrer">
           <div>
             Designed &amp; Built by Brittany Chiang<br></br>
-            Revised by Yashita Namdeo
+            Revised by Oscar Robert-Besle
           </div>
 
           {githubInfo.stars && githubInfo.forks && (
